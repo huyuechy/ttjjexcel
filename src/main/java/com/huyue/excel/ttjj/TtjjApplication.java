6e00a52df30a2c7144fd5a449fc3a95670c3c147
huyue <huyuechy@163.com>
2018-08-23 08:13:48 +0800

    first

---
+++ src/main/java/com/huyue/excel/ttjj/TtjjApplication.java
@@ -0,0 +1,12 @@
+package com.huyue.excel.ttjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtjjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtjjApplication.class, args);
+    }
+}
